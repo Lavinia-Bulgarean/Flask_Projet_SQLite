cab2a38469f602238a739e5954c6caf6ce8e6266
Lavinia-Bulgarean <bulgareanhaiduclavinia@gmail.com>
2025-03-19 12:22:25 +0100

    Update create_dbBiblio.py

--- create_dbBiblio.py
+++ create_dbBiblio.py
@@ -11,19 +11,19 @@ cur = connection.cursor()
 cursor.execute("INSERT INTO Utilisateurs (nom, prenom, email, mot_de_passe, role) VALUES ('Admin', 'Super', 'admin@biblio.com', 'admin123', 'admin')")
 cursor.execute("INSERT INTO Utilisateurs (nom, prenom, email, mot_de_passe, role) VALUES ('Doe', 'John', 'john.doe@gmail.com', 'password', 'utilisateur')")
 
-cursor.execute("INSERT INTO Genres (nom) VALUES ('Science-Fiction')")
-cursor.execute("INSERT INTO Genres (nom) VALUES ('Roman')")
+cur.execute("INSERT INTO Genres (nom) VALUES ('Science-Fiction')")
+cur.execute("INSERT INTO Genres (nom) VALUES ('Roman')")
     
-cursor.execute("INSERT INTO Auteurs (nom, prenom) VALUES ('Asimov', 'Isaac')")
-cursor.execute("INSERT INTO Auteurs (nom, prenom) VALUES ('Hugo', 'Victor')")
+cur.execute("INSERT INTO Auteurs (nom, prenom) VALUES ('Asimov', 'Isaac')")
+cur.execute("INSERT INTO Auteurs (nom, prenom) VALUES ('Hugo', 'Victor')")
 
-cursor.execute("INSERT INTO Livres (titre, id_auteur, id_genre, annee_publication, ISBN) VALUES ('Fondation', 1, 1, 1951, '978-0-123456-47-2')")
-cursor.execute("INSERT INTO Livres (titre, id_auteur, id_genre, annee_publication, ISBN) VALUES ('Les Misérables', 2, 2, 1862, '978-2-123456-78-3')")
+cur.execute("INSERT INTO Livres (titre, id_auteur, id_genre, annee_publication, ISBN) VALUES ('Fondation', 1, 1, 1951, '978-0-123456-47-2')")
+cur.execute("INSERT INTO Livres (titre, id_auteur, id_genre, annee_publication, ISBN) VALUES ('Les Misérables', 2, 2, 1862, '978-2-123456-78-3')")
 
-cursor.execute("INSERT INTO Stock (id_livre, quantite) VALUES (1, 3)")
-cursor.execute("INSERT INTO Stock (id_livre, quantite) VALUES (2, 2)")
+cur.execute("INSERT INTO Stock (id_livre, quantite) VALUES (1, 3)")
+cur.execute("INSERT INTO Stock (id_livre, quantite) VALUES (2, 2)")
 
-cursor.execute("INSERT INTO Emprunts (id_utilisateur, id_livre, date_retour_prevu, statut) VALUES (2, 1, '2024-04-01', 'emprunté')")
+cur.execute("INSERT INTO Emprunts (id_utilisateur, id_livre, date_retour_prevu, statut) VALUES (2, 1, '2024-04-01', 'emprunté')")
 
 connection.commit()
 connection.close()
