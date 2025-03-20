0fc7046445107edb8b98888a1f259020721db574
Lavinia-Bulgarean <bulgareanhaiduclavinia@gmail.com>
2025-03-20 10:11:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -81,41 +81,14 @@ def enregistrer_client():
 def accueil():
     return render_template('accueil.html')
 
-
-# Route de connexion
-@app.route('/connexion', methods=['GET', 'POST'])
-def connexion():
-    if request.method == 'POST':
-        nom = request.form['nom']
-        mot_de_passe = request.form['mot_de_passe']
-        # Connexion à la base de données
-        def get_db():
-        conn = sqlite3.connect('bibliotheque.db')
-        conn.row_factory = sqlite3.Row  # Pour accéder aux colonnes par leur nom
-        return conn
-        
-        conn = get_db()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM Utilisateurs WHERE nom = ? AND mot_de_passe = ?", (nom, mot_de_passe))
-        utilisateur = cursor.fetchone()
-
-        if utilisateur:
-            session['authentifie'] = True
-            session['user_id'] = utilisateur['id']
-            session['role'] = utilisateur['role']
-            return redirect(url_for('index'))
-        else:
-            return render_template('authentification.html', error="Identifiants incorrects")
-    
-    return render_template('authentification.html')
-
-# Route pour la déconnexion
-@app.route('/logout')
-def logout():
-    session.clear()  # Efface la session
-    return redirect(url_for('index'))
-
-
+@app.route('/Nos Livres/')
+def ReadBDD():
+    conn = sqlite3.connect('bibliotheque.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Livres;')
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('read_data.html', data=data)
 
 
 
